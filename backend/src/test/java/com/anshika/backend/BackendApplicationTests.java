ea3e802c1aa96d1fe6aef3f5ace201c5ffd46e46
Anshika220314 <1406pradhananshika@gmail.com>
2026-05-19 02:16:44 +0530

    Add Spring Boot backend structure

---
+++ backend/src/test/java/com/anshika/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshika.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
